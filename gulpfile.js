9740ef04a2ac9280131233e5a97995be6bd30a56
Sahar Jafari <shr.jafari@gmail.com>
2015-10-30 16:51:49 -0700

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var livereload = require('gulp-livereload');
+
+gulp.task('connect', ['watch'], function() {
+  connect.server();
+});
+ 
+gulp.task('default', ['connect']);
+
+gulp.task('html', function() {
+	gulp.src('index.html')
+		.pipe(livereload());
+});
+
+// gulp.task('js', function() {
+// 	gulp.src('./js/*.js')
+// 		.pipe(livereload());
+// });
+
+gulp.task('watch', function() {
+  livereload.listen();
+  gulp.watch('index.html', ['html']);
+  //gulp.watch('js/*.js', ['js']);
+});
