d22d61521b480c7f3a34b42b448d073909dc2080
Sahar Jafari <shr.jafari@gmail.com>
2015-10-30 21:35:12 -0700

    add package.json

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,24 @@ var gulp = require('gulp');
 var connect = require('gulp-connect');
 var livereload = require('gulp-livereload');
 
-gulp.task('connect', ['watch'], function() {
+gulp.task('connect', function() {
   connect.server();
 });
  
 gulp.task('default', ['connect']);
 
-gulp.task('html', function() {
-	gulp.src('index.html')
-		.pipe(livereload());
-});
+// gulp.task('html', function() {
+// 	gulp.src('index.html')
+// 		.pipe(livereload());
+// });
 
 // gulp.task('js', function() {
 // 	gulp.src('./js/*.js')
 // 		.pipe(livereload());
 // });
 
-gulp.task('watch', function() {
-  livereload.listen();
-  gulp.watch('index.html', ['html']);
-  //gulp.watch('js/*.js', ['js']);
-});
+// gulp.task('watch', function() {
+//   livereload.listen();
+//   gulp.watch('index.html', ['html']);
+//   //gulp.watch('js/*.js', ['js']);
+// });
---
+++ script.js
@@ -0,0 +1,4 @@
+'use strict';
+var app = angular.module('T9', ['ngMaterial']);
+
+app.controller('T9Ctrl', function(){});
