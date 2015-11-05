47558e23e1d57dfcce10d9df340a69a6e35cf42b
Sahar Jafari <shr.jafari@gmail.com>
2015-11-04 18:43:27 -0800

    fixed the problem with LESS

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
 var livereload = require('gulp-livereload');
-var less = require('gulp-less')
+var less = require('gulp-less');
+var wiredep = require('wiredep').stream;
 
 gulp.task('connect', ['watch'], function() {
   connect.server();
@@ -9,25 +10,32 @@ gulp.task('connect', ['watch'], function() {
  
 gulp.task('default', ['connect']);
 
-// gulp.task('html', function() {
-// 	gulp.src('index.html')
-// 		.pipe(livereload());
-// });
+gulp.task('bower', function () {
+  gulp.src('./index.html')
+    .pipe(wiredep())
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('html', function() {
+	gulp.src('index.html')
+		.pipe(livereload());
+});
 
 // gulp.task('js', function() {
 // 	gulp.src('./js/*.js')
 // 		.pipe(livereload());
 // });
 
+gulp.task('less', function() {
+  gulp.src('./*.less')
+    .pipe(less())
+    .pipe(gulp.dest('./style'))
+    .pipe(livereload());
+});
+
 gulp.task('watch', function() {
   livereload.listen();
-  
-  //gulp.watch('index.html', ['html']);
+  gulp.watch('./*.less', ['less']);
+  gulp.watch('index.html', ['html']);
   //gulp.watch('js/*.js', ['js']);
-});
-gulp.task('less', function() {
-  gulp.src('*.less')
-    .pipe(less())
-    .pipe(gulp.dest('style'))
-    .pipe(livereload());
 });
